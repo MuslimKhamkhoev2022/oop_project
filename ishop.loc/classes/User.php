6af6a8cac900e329c2800ef9ffc8c66a20524951
muslim <xammuslim06@gmail.com>
2024-02-14 19:49:31 +0300

    Поправил класс User.php, исправил ошибку вывода

--- ishop.loc/classes/User.php
+++ ishop.loc/classes/User.php
@@ -2,7 +2,7 @@
 
 class User
 {
-    private $db;
+    private $db, $data;
 
     public function __construct()
     {
@@ -17,9 +17,23 @@ class User
     public function login($email = null, $password = null)
     {
         if ($email) {
-            $user = $this->db->get('users', ['email', '=', $email])->first();
-            var_dump($user->password);
-            exit;
+            $user = $this->find($email);
+            if ($user){
+                if (password_verify($password, $user[0]->password)){
+                    Session::put('user', $user[0]->id);
+                    return true;
+                }
+            }
         }
+        return false;
+    }
+
+    public function find($email = null)
+    {
+        $this->data = $this->db->get('users', ['email', '=', $email])->first();
+        if ($this->data){
+            return true;
+        }
+        return false;
     }
 }
--- ishop.loc/index.php
+++ ishop.loc/index.php
@@ -1,8 +1,5 @@
 <?php
 require_once 'init.php';
 
-//var_dump(Session::get(Config::get('session.user_session')));
-
-$user = new User;
-echo $user->data()->username;
+var_dump(Session::get('user'));
 
